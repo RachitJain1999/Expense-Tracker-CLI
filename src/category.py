cd5b14cb50f7f8091e2fb7f6a48ceb90c0e9b7f2
Your Name <49223863+RachitJain1999@users.noreply.github.com>
2024-11-04 03:01:30 +0530

    initial commit , folder structure and basic classes

---
+++ src/category.py
@@ -0,0 +1,5 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+    def view(self):
+        print(f"Category: {self.name}")
---
+++ src/main.py
@@ -0,0 +1,29 @@
+
+def main():
+    print("Welcome to the Expense Tracker Application! \U0001F600")  
+    print("Hello! \U0001F44B")  
+    print("Let's get started! \U0001F680")  
+
+    while True:
+        print("Expense Tracker Menu:")
+        print("1. Add Expense")
+        print("2. Show All Expenses")
+        print("3. Add Category")
+        print("4. Show All Categories")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            print("Exiting Expense Tracker. See You Again!")
+            break
+        else:
+            print("Invalid choice :( ") 
+            print("Please enter a valid choice ")
